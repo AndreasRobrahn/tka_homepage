a3bc1d884f3b65ad5e16863def83b0247b079d3f
AndreasRobrahn <anno1984@gmx.de>
2021-06-25 17:03:22 +0200

    fix

--- resources/views/uk_layout.blade.php
+++ resources/views/uk_layout.blade.php
@@ -295,7 +295,64 @@
         </div>
       </div>
       </div>
-
+      <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Deine Angaben</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="toggleContactModal()">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form class="" action="{{route('notification.customer')}}" method="post">
+              @csrf
+              <div class="row m-0 mt-2">
+                <div class="col-sm">
+                  <div class="form-group">
+                    <label for="surname">Vorname</label>
+                    <input type="text" class="form-control input1 " name="surname" value="" id="surname">
+                  </div>
+                </div>
+                <div class="col-sm">
+                  <div class="form-group">
+                    <label for="lastname">Nachname</label>
+                    <input type="text" class="form-control input1" name="lastname" value="" id="lastname">
+                  </div>
+                </div>
+              </div>
+              <div class="row m-0 mt-2">
+                <div class="col">
+                  <div class="form-group">
+                  <label for="email">Emailadresse</label>
+                    <input type="text" class="form-control input1" name="email" value="" required id="email">
+                </div>
+              </div>
+              </div>
+              <div class="row m-0 mt-2">
+                <div class="col">
+                  <div class="form-group">
+                    <label for="phone">Telefonnummer für eine Rückmeldung</label>
+                    <input type="text" class="form-control input1" name="phone" value="" id="phone">
+                  </div>
+                </div>
+                </div>
+              <div class="row m-0 mt-2">
+                <div class="col">
+                  <div class="form-group">
+                    <h5>Was stellst du dir vor?</h5>
+                    <textarea name="message" class="form-control input1" rows="8" cols="80" style="width: 100%;"></textarea>
+                  </div>
+                </div>
+              </div>
+              <div class="row m-2">
+                <button type="submit" name="button" class="btn btn-success btn-block btn-outline">Absenden</button>
+              </div>
+            </form>
+          </div>
+          </div>
+        </div>
+      </div>
   </body>
     <script  src="{{asset('js/main.js')}}"></script>
   </html>
